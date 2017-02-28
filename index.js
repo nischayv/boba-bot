d80dc5d5b66f5511ec7cf5d46ca1754d97c4edbf
Nischay Venkatram <nischay.v@gmail.com>
2017-02-28 00:38:19 -0800

    Updated stuff

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-const express = require('express');
+const botkit = require('botkit')
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const slackClient = require('./slackClient');
-const routes = require('./routes');
-app.use('/', routes);
-
 const slackToken = process.env.SLACK_TOKEN;
-const slackLogLevel = 'verbose';
-const witToken = process.env.WIT_TOKEN;
-
-rtm.start();
 
 
-slackClient.addAuthenticatedHandler(rtm, () => {
-    app.listen(port);
-    console.log(`Kyp is listening on port ${port} in dev mode`);
-});
 
 exports = module.exports = app;
