4e03fc09b6a3975af517078b9c852a3a90792f14
Nischay Venkatram <nischay.v@gmail.com>
2017-03-01 13:27:50 -0800

    Added some bot commands

--- index.js
+++ index.js
@@ -1,19 +1,96 @@
 const Botkit = require('botkit')
 const os = require('os');
+const wiki = require('./wiki')
 
 const slackToken = process.env.SLACK_TOKEN;
-if(!slackToken) {
-  process.exit(1);
+if (!slackToken) {
+    process.exit(1);
 }
 
 const controller = Botkit.slackbot({
-  debug: true,
+    debug: true
 });
 
 const bot = controller.spawn({
-  token: slackToken
+    token: slackToken
+}).startRTM();
+
+controller.hears(['shutdown'], 'direct_message,direct_mention', (bot, message) => {
+
+    bot.startConversation(message, (err, convo) => {
+
+        convo.ask('Are you sure you want me to shutdown?', [{
+                pattern: bot.utterances.yes,
+                callback: (response, convo) => {
+                    convo.say('Bye!');
+                    convo.next();
+                    setTimeout(function() {
+                        process.exit();
+                    }, 3000);
+                }
+            },
+            {
+                pattern: bot.utterances.no,
+                default: true,
+                callback: (response, convo) => {
+                    convo.say('*Phew!*');
+                    convo.next();
+                }
+            }
+        ]);
+    });
 });
 
-bot.startRTM((err, bot, payload) => {
+controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
+    'direct_message,direct_mention',
+    (bot, message) => {
+        var hostname = os.hostname();
+        var uptime = formatUptime(process.uptime());
 
+        bot.reply(message,
+            ':robot_face: I am a bot named <@' + bot.identity.name +
+            '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+    });
+
+controller.hears(['hello', 'hi', 'hey', 'yo'], 'direct_message,direct_mention', (bot, message) => {
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'robot_face',
+    }, (err, res) => {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
+
+    controller.storage.users.get(message.user, (err, user) => {
+        if (user && user.name) {
+            bot.reply(message, 'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message, 'Hello.');
+        }
+    });
 });
+
+controller.hears([''], 'direct_message,direct_mention', (bot, message) => {
+    const response = wiki(message);
+    bot.reply(response);
+});
+
+function formatUptime(uptime) {
+    var unit = 'second';
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'minute';
+    }
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'hour';
+    }
+    if (uptime != 1) {
+        unit = unit + 's';
+    }
+
+    uptime = uptime + ' ' + unit;
+    return uptime;
+}
--- wiki.js
+++ wiki.js
@@ -1,14 +1,15 @@
 const request = require('request')
+const test = require('./wiki')
 
 const url = 'https://en.wikipedia.org/wiki/';
 
-module.exports = function (text) {
-  const modifiedText = text.replace(' ', '_'); 
+module.exports = function(text) {
+    const modifiedText = text.replace(' ', '_');
 
-  request(url, function(err, response, html) {
-      if(!err) {
-          const $ = cheerio.load(html);
+    request(url, function(err, response, html) {
+        if (!err) {
+            const $ = cheerio.load(html);
 
-      }
-  });
+        }
+    });
 }
