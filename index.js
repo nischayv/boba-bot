f51b772bddfcb6605874c8ad5945087a627f7fbc
Nischay Venkatram <nischay.v@gmail.com>
2017-05-01 00:39:16 -0700

    Added clever bot

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Botkit = require('botkit')
+const Cleverbot = require('cleverbot-node')
 const os = require('os')
 const apiai = require('apiai')
 const wiki = require('./wiki')
@@ -6,104 +7,110 @@ const timeUtil = require('./utils/timeUtil')
 
 const slackToken = process.env.SLACK_TOKEN;
 const apiaiToken = process.env.APIAI_TOKEN;
-if (!slackToken || !apiaiToken) {
-    process.exit(1);
+const cleverbotToken = process.env.CLEVER_TOKEN;
+
+if (!slackToken || !apiaiToken || !cleverbotToken) {
+  process.exit(1);
 }
 const ai = apiai(apiaiToken);
-
+const cleverbot = new Cleverbot;
+cleverbot.configure({ botapi: cleverbotToken });
 const controller = Botkit.slackbot({
-    debug: true,
-    stats_optout: true
+  debug: true,
+  stats_optout: true
 });
 
 const bot = controller.spawn({
-    token: slackToken
+  token: slackToken
 }).startRTM((err) => {
-    if (err) {
-        throw new Error('Could not connect to Slack');
-    }
+  if (err) {
+    throw new Error('Could not connect to Slack');
+  }
 });
 
 controller.hears(['shutdown'], 'direct_message,direct_mention', (bot, message) => {
-    bot.startConversation(message, (err, convo) => {
+  bot.startConversation(message, (err, convo) => {
 
-        convo.askWithTyping('Are you sure you want me to shutdown?', [{
-                pattern: bot.utterances.yes,
-                callback: (response, convo) => {
-                    convo.askWithTyping(`What's the passcode??`, [{
-                            pattern: 'Davy Jones',
-                            callback: (response, convo) => {
-                                convo.sayWithTyping('Roger Roger!');
-                                convo.next();
-                                setTimeout(() => {
-                                    bot.closeRTM();
-                                }, 3000);
-                            }
-                        },
-                        {
-                            pattern: '.*',
-                            default: true,
-                            callback: (response, convo) => {
-                                convo.sayWithTyping('Wrong passcode, Jedi scum');
-                                convo.next();
-                            }
-                        }
-                    ]);
-                    convo.next();
-                }
+    convo.askWithTyping('Are you sure you want me to shutdown?', [{
+        pattern: bot.utterances.yes,
+        callback: (response, convo) => {
+          convo.askWithTyping(`What's the passcode??`, [{
+              pattern: 'Davy Jones',
+              callback: (response, convo) => {
+                convo.sayWithTyping('Roger Roger!');
+                convo.next();
+                setTimeout(() => {
+                  bot.closeRTM();
+                }, 3000);
+              }
             },
             {
-                pattern: bot.utterances.no,
-                default: true,
-                callback: (response, convo) => {
-                    convo.say('*Phew!*');
-                    convo.next();
-                }
+              pattern: '.*',
+              default: true,
+              callback: (response, convo) => {
+                convo.sayWithTyping('Wrong passcode, Jedi scum');
+                convo.next();
+              }
             }
-        ]);
-    });
+          ]);
+          convo.next();
+        }
+      },
+      {
+        pattern: bot.utterances.no,
+        default: true,
+        callback: (response, convo) => {
+          convo.say('*Phew!*');
+          convo.next();
+        }
+      }
+    ]);
+  });
 });
 
 controller.hears(['uptime', 'identify yourself', 'who are you.*', 'what are you.*'],
-    'direct_message,direct_mention',
-    (bot, message) => {
-        bot.reply(message, {
-            type: 'typing'
-        })
-        const hostname = os.hostname();
-        const uptime = timeUtil.formatUptime(process.uptime());
+  'direct_message,direct_mention',
+  (bot, message) => {
+    bot.reply(message, {
+      type: 'typing'
+    })
+    const hostname = os.hostname();
+    const uptime = timeUtil.formatUptime(process.uptime());
 
-        bot.replyWithTyping(message,
-            ':robot_face: I am a bot named <@' + bot.identity.name +
-            '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-    });
+    bot.replyWithTyping(message,
+      ':robot_face: I am a bot named <@' + bot.identity.name +
+      '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+  });
 
 controller.hears(['.*'], 'direct_message,direct_mention', (bot, message) => {
-    bot.reply(message, {
-        type: 'typing'
-    })
-    const request = ai.textRequest(message.text, {
-        sessionId: 'e1c8a397-4d65-4e87-977f-00f1f505169e'
-    });
+  bot.reply(message, {
+    type: 'typing'
+  })
+  const request = ai.textRequest(message.text, {
+    sessionId: 'e1c8a397-4d65-4e87-977f-00f1f505169e'
+  });
 
-    request.on('response', (response) => {
-        bot.botkit.log(JSON.stringify(response));
-        const { result: { action, fulfillment, parameters } } = response;
-        if (action === 'wiki') {
-            wiki.ask(parameters.any, (err, res) => {
-                if (err) {
-                    return bot.reply(message, `I'm really sorry, I couldn't find a good answer!`);
-                }
-                bot.reply(message, res);
-            })
+  request.on('response', (response) => {
+    bot.botkit.log(JSON.stringify(response));
+    const { result: { action, fulfillment, parameters } } = response;
+    if (action === 'wiki') {
+      wiki.ask(parameters.any, (err, res) => {
+        if (err || res.includes('Other reasons this message may be displayed:\n')) {
+          cleverbot.write(message.text, function(cleverResponse) {
+            bot.reply(message, cleverResponse.output);
+          });
         } else {
-            bot.reply(message, fulfillment.speech);
+          bot.reply(message, res);
         }
-    });
+      })
+    } else {
+      bot.reply(message, fulfillment.speech);
+    }
+  });
 
-    request.on('error', (error) => {
-        bot.botkit.log(error);
-    });
+  request.on('error', (error) => {
+    bot.botkit.log(error);
+  });
 
-    request.end();
+  request.end();
 });
