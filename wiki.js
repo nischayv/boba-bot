0258ee01239100f85b2b8b59aedce31310e0c34c
Nischay Venkatram <nischay.v@gmail.com>
2017-05-02 00:38:07 -0700

    Remove wikipedia link

--- wiki.js
+++ wiki.js
@@ -4,17 +4,16 @@ const cheerio = require('cheerio')
 const url = 'https://en.wikipedia.org/wiki/';
 
 function ask(text, cb) {
-    const modifiedText = text.replace(' ', '_');
-    const mainUrl = `${url}${modifiedText}`
-    request(mainUrl, (err, res, html) => {
-        if (err) {
-            return cb(err, null);
-        }
-        const $ = cheerio.load(html);
-        const basic = $('#mw-content-text').find('p').first().text();
-        const response = `${basic} For more information go to ${mainUrl}`;
-        return cb(null, response);
-    });
+  const modifiedText = text.replace(' ', '_');
+  const mainUrl = `${url}${modifiedText}`
+  request(mainUrl, (err, res, html) => {
+    if (err) {
+      return cb(err, null);
+    }
+    const $ = cheerio.load(html);
+    const basic = $('#mw-content-text').find('p').first().text();
+    return cb(null, basic);
+  });
 }
 
 module.exports.ask = ask;
